7f3e4c3fd79e2a48ae318d6158a4166e06a61b8d
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2023-07-07 23:12:50 -0300

    finalizing project

--- js/script.js
+++ js/script.js
@@ -11,3 +11,19 @@ button.addEventListener('click', () => {
     alert("Seu contato foi enviado com sucesso.")
     
 })
+
+/*
+Ex.: para a utilização da foto, utilizei xxx, para o nome, utilizei a tag html xxx da seguinte forma... Para a descrição, usei... Para os links, usei... Para o menu, usei...
+
+Para criação do Menu usei a tag header, nav
+coloquei dentro de uma ul a lista de tópicos para o menu
+
+deixei a imagem e o nome em uma div separada da tag nav para ficar mais facil trabalhar a imagem caso houvesse necessidade
+
+usei a tag details para esconder algumas informações
+
+Comecei desenvolvendo para mobile first e depois fui adaptando para telas maiores, 
+Para a simulação de envios de dados usei o DOM, 
+Para a const a (está com um escutador de evento, que ao ser clicado, mostra na tela um paragrafo com as informações 'quem sou')
+e para o button o escutador de enventos está para exibir um alerta na tela para o usuário.
+*/
