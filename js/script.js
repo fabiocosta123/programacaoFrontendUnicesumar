df027e8d718d3f9aa91f8b6cc1876a11837c9492
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2023-07-07 22:52:08 -0300

    create paragraph into Dom

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 const button = document.querySelector('button')
+const a = document.querySelector('#link_quem_sou')
+const p = document.querySelector('.quem_sou')
 
+a.addEventListener('click', () =>{
+    p.textContent = "Casado, pai de 3 filhos, apaixonado pela familia e por tecnologia, auto-didata nas linguagens de programação."
+})
 
 button.addEventListener('click', () => {
     
